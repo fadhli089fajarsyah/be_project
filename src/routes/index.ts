e53c86a6d68b2749d21e57555f89688d479a0acf
fadhli_fajarsyah <fadhlifajarsyah089@gmail.com>
2024-03-06 07:44:01 +0700

    1

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,17 +1,18 @@
 import * as express from "express";
 import UserControllers from "../controllers/UserControllers";
-import paslonService from "../controllers/paslon";
+import paslon from "../controllers/paslon";
+import beritaControllers from "../controllers/berita.Controllers";
 
 
 const Route = express.Router()
 //menampilkan data
 Route.get("/users", UserControllers.find)
-Route.get("/paslonAdds",paslonService.find )
-Route.get("/beritas",paslonService.find )
+Route.get("/paslonAdds",paslon.find )
+Route.get("/beritas",beritaControllers.find )
 
 // buat menambah data
 Route.post("/user", UserControllers.create)
-Route.post("/paslonAdd", paslonService.create)
-Route.post("/beritasAdd", paslonService.create)
+Route.post("/paslonAdd", paslon.create)
+Route.post("/beritaAdd", beritaControllers.create)
 
 export default Route
